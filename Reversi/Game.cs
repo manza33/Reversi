c6372ecb6b1b765d49aec509b18804874b999898
Laetitia B <bourgois.laetitia@gmail.com>
2020-03-23 17:48:53 +0100

    Affichage du jeu

---
+++ Reversi/Game.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+
+namespace Reversi
+{
+    public class Game
+    {
+
+        private int[,] _board = new int[8,8];
+
+        public Game()
+        {
+            _board[3, 3] = 1; _board[4, 3] = -1;
+            _board[3, 4] = -1; _board[4, 4] = 1;
+
+            //Console.WriteLine($"Nombre Cols : {_board.Rank} ");
+            //Console.WriteLine($"Nombre Rows : {_board.GetLength(1)} ");
+        }
+
+        public int GetTile(int col, int row)
+        {
+            return _board[col, row];            
+        }
+
+        public int BoardRows => _board.GetLength(0);
+        public int BoardCols => _board.GetLength(1);
+
+    }
+}
---
+++ ReversiClient/Program.cs
@@ -0,0 +1,89 @@
+﻿using Colorful;
+using Reversi;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using Console = Colorful.Console;
+
+namespace ReversiClient
+{
+    class Program
+    {
+        //private const string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        
+
+        static void Main(string[] args)
+        {
+            //Console.OutputEncoding = Encoding.Unicode;
+
+            Formatter[] Tokens = new Formatter[]
+            {
+                new Formatter(" ", Color.White),
+                new Formatter("■", Color.MediumAquamarine),
+                new Formatter("■", Color.HotPink)
+            };
+
+            Game newGame = new Game();
+
+            static void WriteALine()
+            {
+                string Line = "";
+                for (int j = 0; j < 8; j++) // >> methode
+                {
+                    Line += "+---";
+                }
+                Line += "+";
+                Console.WriteLine(Line);
+            }
+
+            #region : Affichage du jeu
+
+            Console.Write("   ");
+            for (int i = 0; i < newGame.BoardCols; i++)
+            {
+                Console.Write($"  {(char)('A' + i)} ");
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < newGame.BoardRows; i++)
+            {
+                Console.Write($"   ");
+                WriteALine();
+
+                Console.Write($" {i + 1} ");
+                for (int j = 0; j < newGame.BoardCols; j++)
+                {
+                    var token = 0;
+                    if (newGame.GetTile(i, j) == 1)
+                    {
+                        token = 1;
+                    }
+                    else if (newGame.GetTile(i, j) == -1)
+                    {
+                        token = 2;
+                    }
+                    Console.WriteFormatted($"| {{{token}}} ", Color.White, Tokens);
+
+                }
+                Console.WriteLine("|");
+
+            }
+            Console.Write($"   ");
+            WriteALine();
+
+            #endregion
+
+            Console.WriteLine("Joueur 1 : Veuillez choisir une case");
+            var TileSelected = Console.ReadLine();
+
+            var coord = TileSelected.Split("");
+            if(coord.Length > 2)
+            {
+                Console.WriteLine("erreur");
+            }
+
+        }
+    }
+}
